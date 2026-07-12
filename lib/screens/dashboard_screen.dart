ff25ef0872d9a3b5368bff60fe1b81a119095fdf
Piyush Assudani <piyushassudani96@gmail.com>
2026-07-12 22:55:28 +0530

    style: remove filters button from dashboard home tab

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -581,7 +581,6 @@ class _DashboardScreenState extends State<DashboardScreen> with WidgetsBindingOb
     return Column(
       children: [
         _buildPremiumHeader(),
-        _buildPremiumFiltersRow(),
         Expanded(
           child: RefreshIndicator(
             onRefresh: () => provider.fetchDailyPicks(refresh: true, filters: _getHomeFilters()),
